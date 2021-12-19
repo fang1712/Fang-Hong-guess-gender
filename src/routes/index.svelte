004d5100ec45489b92da20733d2b6d76545e5228
fang1712 <fanghong1712@gmail.com>
2021-12-19 04:15:29 +0000

    Completed

---
+++ src/routes/index.svelte
@@ -0,0 +1,22 @@
+<script>
+    let name = "";
+    let gender = "";
+   
+    async function guessgender() {
+  fetch(' https://api.genderize.io/?name=' + name)
+	.then((response) => response.json())
+	.then((data) => {
+  	gender=data.gender;
+	});
+}
+</script>
+
+<h1>Guess Gender</h1>
+
+<form on:submit|preventDefault={guessgender}>
+    <label for="name">Name:</label>
+    <input id="name" bind:value={name} type="text" />
+    <button type="submit">Check</button>
+</form>
+<p>Gender: {gender}</p>
+
